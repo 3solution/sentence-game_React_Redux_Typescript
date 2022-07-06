e12e67f9bbb708007a332e0cb4da2dc8ede5e97e
Evil-Developer <pro.expert0525@gmail.com>
2022-07-06 05:48:46 +0300

    Fix: change position when and where

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -19,9 +19,9 @@ const MainPage = () => {
       <Input label={'What'} className={styles.input} />
       <Input label={'When'} className={styles.input} />
       <Input label={'Where'} className={styles.input} />
-      <p className={styles.sentence}>{`${words.who.charAt(0).toUpperCase() + words.who.slice(1)}  ${words.what}  ${
-        words.when
-      }  ${words.where}.`}</p>
+      <p className={styles.sentence}>{`${words.who.charAt(0).toUpperCase() + words.who.slice(1)}  ${words.what} ${
+        words.where
+      }  ${words.when}  .`}</p>
       <button className={styles.button} onClick={clear}>
         New Game
       </button>
